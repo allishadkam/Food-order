be4d6c918a1d03725a65183a07484582bf4324ab
ali shadkam <alishadkam007@gmail.com>
2023-08-16 20:06:53 +0430

    meals part interface section

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import classes from './AvailableMeals.module.css';
-
+import Card from '../UI/Card';
+import MealItem from './MealItem';
 const DUMMY_MEALS = [
   {
     id: 'm1',
@@ -28,11 +29,14 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} price={meal.price} description={meal.description} name={meal.name}/>);
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+      
     </section>
   );
 };
