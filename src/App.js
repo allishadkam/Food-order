ef8c9d8aef3a9dd12e0e72964a63f69cfafdf5c4
ali shadkam <alishadkam007@gmail.com>
2023-08-17 21:23:34 +0430

    creating back drop and portal and layouts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
+import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import classes from './Cart.module.css'
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
+
 const Cart = (props) => {
-    const cartItems = (
-        <ul className={classes['cart-items']}>
-          {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-            <li>{item.name}</li>
-          ))}
-        </ul>
-      );
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -19,11 +20,9 @@ const Cart = (props) => {
       <div className={classes.actions}>
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
-
       </div>
-    </div>
-  )
-}
+    </Modal>
+  );
+};
 
-export default Cart
-props
+export default Cart;
