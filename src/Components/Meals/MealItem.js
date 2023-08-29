d1c934e350f65a96704309dbad0c122c3cb2c9ca
ali shadkam <alishadkam007@gmail.com>
2023-08-29 22:53:27 +0430

    bug fix

--- src/Components/Meals/MealItem.js
+++ src/Components/Meals/MealItem.js
@@ -18,14 +18,14 @@ const MealItem = (props) => {
     };
 
   return (
-    <li className={classes.meals} >
+    <li className={classes.meal} >
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-            <MealItemForm onAddToCart={addToCartHandler}/>
+        <MealItemForm onAddToCart={addToCartHandler}/>
       </div>
     </li>
   )
